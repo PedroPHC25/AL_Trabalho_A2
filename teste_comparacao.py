78d520d099942250c0843c6a4ea6cc2f26fb0f2c
Pedro H. Coterli <pedrocoterli@gmail.com>
2023-11-11 16:55:32 -0300

    Funcionou, porra!

--- teste_comparacao.py
+++ teste_comparacao.py
@@ -18,7 +18,7 @@ def create_base_matrix(folder):
     # print(train_famous_np_matrix, train_famous_np_matrix.shape)
     mean_train_face = np.mean(train_famous_np_matrix, axis=0)
     # print(mean_train_face, mean_train_face.shape)
-    centered_train_face = (train_famous_np_matrix - mean_train_face)/np.std(train_famous_np_matrix)
+    centered_train_face = (train_famous_np_matrix - mean_train_face)
     # print(centered_train_face, centered_train_face.shape)
     V_Face_Famous = np.linalg.svd(centered_train_face, full_matrices=False)[2]
     return mean_train_face, V_Face_Famous
@@ -35,6 +35,7 @@ def projection(image, matrix, mean):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = np.array(img)
     img = img.flatten()
+    img = img - np.mean(img)
     coordinates = np.dot(matrix, img)
     new_img = np.dot(matrix.T, coordinates)
     # print(new_img)
