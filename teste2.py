4acc81daf4e682df2177eca81bd3ee8035bc6e46
Pedro H. Coterli <pedrocoterli@gmail.com>
2023-11-11 14:33:37 -0300

    Separei os artistas em pastas

--- teste2.py
+++ teste2.py
@@ -1,63 +1,24 @@
+import os
 import cv2
-from cv2 import readI
+import numpy as np
 
-if __name__ == '__main__':
- 
-  # Number of EigenFaces
-  NUM_EIGEN_FACES = 10
- 
-  # Maximum weight
-  MAX_SLIDER_VALUE = 255
- 
-  # Directory containing images
-  dirName = "images"
- 
-  # Read images
-  images = readImages(dirName)
- 
-  # Size of images
-  sz = images[0].shape
- 
-  # Create data matrix for PCA.
-  data = createDataMatrix(images)
- 
-  # Compute the eigenvectors from the stack of images created
-  print("Calculating PCA ", end="...")
-  mean, eigenVectors = cv2.PCACompute(data, mean=None, maxComponents=NUM_EIGEN_FACES)
-  print ("DONE")
- 
-  averageFace = mean.reshape(sz)
- 
-  eigenFaces = []; 
- 
-  for eigenVector in eigenVectors:
-    eigenFace = eigenVector.reshape(sz)
-    eigenFaces.append(eigenFace)
- 
-  # Create window for displaying Mean Face
-  cv2.namedWindow("Result", cv2.WINDOW_AUTOSIZE)
- 
-  # Display result at 2x size
-  output = cv2.resize(averageFace, (0,0), fx=2, fy=2)
-  cv2.imshow("Result", output)
- 
-  # Create Window for trackbars
-  cv2.namedWindow("Trackbars", cv2.WINDOW_AUTOSIZE)
- 
-  sliderValues = []
- 
-  # Create Trackbars
-  for i in xrange(0, NUM_EIGEN_FACES):
-    sliderValues.append(MAX_SLIDER_VALUE/2)
-    cv2.createTrackbar( "Weight" + str(i), "Trackbars", MAX_SLIDER_VALUE/2, MAX_SLIDER_VALUE, createNewFace)
- 
-  # You can reset the sliders by clicking on the mean image.
-  cv2.setMouseCallback("Result", resetSliderValues);
- 
-  print('''Usage:
-  Change the weights using the sliders
-  Click on the result window to reset sliders
-  Hit ESC to terminate program.''')
- 
-  cv2.waitKey(0)
-  cv2.destroyAllWindows()
+# Criando o path para onde as iamgens estão
+famousim = "imagens_artistas/"
+# Criando uma lista para adicionar as imagens
+famous_images = []
+
+# Fazendo um for loop para iterar sobre todas as imagens
+for image in (os.listdir(famousim)): 
+    # Adionanco o path com cada imagem para termos o arquivo da imgens
+    path = os.path.join(famousim, image)
+    # Lendo a imagem
+    img = cv2.imread(path)
+    # Transformando a imagem em preto e branco 
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Adicionando a imgem na lista
+    famous_images.append(img)
+
+# Transformando a lista num array
+famous_images_np = np.array(famous_images)
+# print(famous_images_np.shape)
+
